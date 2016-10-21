400a2d42ff417749b82db66922a6a7ff0f076a8a
Anknit <Anknit@af72691c-e56f-4e9a-8dd1-63cc30f88d2b>
2016-10-21 19:58:14 +0000

    Mobile verification implemented

--- trunk/www/js/verifyCode.js
+++ trunk/www/js/verifyCode.js
@@ -17,22 +17,30 @@ function watchCodeSMS(){
     },true);
 }
 
-function resendCodeRequest(){
-    var mobileNumber = localStorage.getItem("mobilenum");
+(function onInit() {
+    var mobileNum = localStorage.getItem("mobilenum");
+    if (typeof mobileNum !== "undefined") {
+        document.getElementById('mobile-num').value = mobileNum;
+    }
+}());
+
+function getActivationCode(){
+    var mobileNumber = document.getElementById('mobile-num').value;
+    localStorage.setItem('mobilenum', mobileNumber);
     var userName = localStorage.getItem("username");
 	var data = {CodeType:"PhoneCode",MobileNumber:mobileNumber,UserName:userName}; 
-
 	var data1 = JSON.stringify(data);
 	var http = new XMLHttpRequest();
-	http.open("POST",authServiceBase+'api/accounts/sendcode',true);
+	http.open("POST",authServiceBase+'api/accounts/getcode',true);
 	http.setRequestHeader("Content-Type","application/json");
 	http.onreadystatechange=function(){
 		if(http.readyState == 4){
             if(http.status==200){
-               document.getElementById('code-input').value = '';
+                document.getElementById('code-input').value = '';
+                document.getElementById('verify-code-container').style.display = 'block';
             }
             else{
-                alert("Error in sending request for resend code");
+                alert("Error in getting verification code");
             }
         }
 	}
@@ -43,8 +51,8 @@ function onActivationCode() {
     var verifyCode = document.getElementById('code-input').value;
     var mobileNumber = localStorage.getItem("mobilenum");
     var userName = localStorage.getItem("username");
-    if(verifyCode == null || verifyCode == "" || verifyCode.length != 4) {
-        alert("Please enter 4 digit code");
+    if(verifyCode == null || verifyCode == "" || verifyCode.length != 6) {
+        alert("Please enter 6 digit code");
         return false;
     }
 	var data = {Code:"",MobileNumber:mobileNumber,UserName:userName}; 
@@ -57,6 +65,7 @@ function onActivationCode() {
 	http.onreadystatechange=function(){
 		if(http.readyState == 4){
             if(http.status==200){
+                localStorage.setItem('isMobileVerified', "true");
                 window.location.href = "./verificationSuccess.html"
             }
             else{
